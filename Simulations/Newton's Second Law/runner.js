f2637e9cdb756c96e5e972103f3a64cb1019348e
Max Ratmeyer <maxratmeyer@Maxs-MacBook-Pro.local>
2019-07-02 12:31:38 -0400

    Added Slider

--- Simulations/Newton's Second Law/runner.js
+++ Simulations/Newton's Second Law/runner.js
@@ -3,15 +3,25 @@ var meterStick;
 
 var Dimensions;
 
-function setup () {
-  Dimensions = Object.freeze({"width": windowWidth * 0.8, "height": windowHeight * 0.8});
-  console.log(Dimensions.width/10);
+var slider;
+
+function setup() {
+  Dimensions = Object.freeze({
+    "width": windowWidth * 0.8,
+    "height": windowHeight * 0.8
+  });
+
+  slider = createSlider(0, 255, 100);
+  slider.position(10, 10);
+  slider.style('width', '80px');
+  
+  console.log(Dimensions.width / 10);
   createCanvas(Dimensions.width, Dimensions.height);
-  meterStick = new MeterStick(Dimensions.width/10, Dimensions.height - 120, Dimensions.width - (Dimensions.width/10)*2, 50);
-  particle = new Particle(Dimensions.width/10, Dimensions.height - 150, 50);
+  meterStick = new MeterStick(Dimensions.width / 10, Dimensions.height - 120, Dimensions.width - (Dimensions.width / 10) * 2, 50);
+  particle = new Particle(Dimensions.width / 10, Dimensions.height - 150, 50);
 }
 
-function draw () {
+function draw() {
   background('#EAEAEB');
   meterStick.create();
   particle.create();
